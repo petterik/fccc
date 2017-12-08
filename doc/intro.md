7544c766bf0980f393c43a9726da9ff4eb82a681
Petter Eriksson <ipetterik@gmail.com>
2017-12-08 11:39:47 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fccc
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/fccc/core_test.clj
@@ -0,0 +1,7 @@
+(ns fccc.core-test
+  (:require [clojure.test :refer :all]
+            [fccc.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
